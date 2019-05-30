9874460a2c32518f53359c2886f3366a7e338359
lxq <1174497004@qqcom>
2019-05-30 11:19:59 +0800

    状态模式例子和测试

---
+++ src/main/java/com/statuspattern/Status.java
@@ -0,0 +1,12 @@
+package com.statuspattern;
+
+public interface Status {
+    //抗炸药
+    void carry();
+    //放置炸药
+    void put();
+    //跑
+    void goaway();
+    //引爆
+    void ignite();
+}
---
+++ src/main/java/com/statuspattern/StatusTest.java
@@ -0,0 +1,25 @@
+package com.statuspattern;
+
+/**
+ * @program: lxq
+ * @description: 状态模式测试
+ * @author: Mr.lxq
+ * @create: 2019-05-30 11:17
+ **/
+
+public class StatusTest {
+
+    public static void main(String[] args) {
+        Context context = new Context();
+
+        context.setStatus(new PutingState());
+
+
+        context.carry();
+
+        context.put();
+    }
+
+
+
+}
