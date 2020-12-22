7386e9719be44583ad4e0b47efe7920a86174d6e
lixiangqian <1174497004@qq.com>
2020-12-22 17:29:27 +0800

    使用静态方法，实现类似单例的功能。

---
+++ src/main/java/com/designpattern/SingletonDesignPattern/staticMethod/Logger.java
@@ -0,0 +1,18 @@
+package com.designpattern.SingletonDesignPattern.staticMethod;
+
+/**
+ * @Author: lxq
+ * @Date: 2020/12/22 17:26
+ */
+public class Logger {
+
+    public static void doSomeThing() {
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
